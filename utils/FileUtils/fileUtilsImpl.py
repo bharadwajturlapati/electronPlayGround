bc1d396bb4729c1b469eeac4ba2f2ee9ccfc2426
yasasvee <yasasvee.atreya@gmail.com>
2016-08-18 09:33:28 +0530

    Added file utils: Remove directory; Create Directory

--- utils/FileUtils/fileUtilsImpl.py
+++ utils/FileUtils/fileUtilsImpl.py
@@ -9,6 +9,7 @@ class FileUtilsImpl:
         self.type = type
         self.name = name
         self.operation = operation
+    
     def createFile(self,isRender,template,data):
         fileObject = open(self.path+'/'+self.name,"a+")
         if isRender:
@@ -17,10 +18,19 @@ class FileUtilsImpl:
             fileObject.write(data)
         fileObject.close()
         return
+    
     def deleteFile(self,path):
         osutils.remove(path)
         return
+
     def deleteFolder(self,path):
+        """ Shutil, Removes a folder and all of its contents, including directories. 
+            Fails for readonly files."""
+        shutil.rmtree(path)
         return
+
     def createFolder(self,path):
+        """ Create a Directory if it does not exist. Suports recursive create. """
+        if not osutils.path.exists(path):
+            osutils.makedirs(path)
         return
