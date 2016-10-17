17c4cd030fe25e769dee5555e549558ab70dd043
bharadwaj.turlapati <bharadwaj.turlapati@kony.com>
2016-10-17 13:21:35 +0530

    Moving away from type script

--- ts/com/apptromix/utils/shellUtils/shellTest.ts
+++
@@ -1,6 +0,0 @@
-const {shell} = require('electron')
-
-//shell.openExternal('https://github.com')
-//opens up this url in users default browser
-//can be used to indicate documentation help.
-shell.showItemInFolder('E:\electronWS\electronPlayGround\components')
